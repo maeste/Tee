833506951fd9070b29f26deda96182f83dca8642
maestri <maestri@171577af-f0f8-0310-8ebc-b7c84246484d>
2005-07-21 13:28:14 +0000

    git-svn-id: svn+ssh://svn.lince.it/svn/Tee/trunk@27 171577af-f0f8-0310-8ebc-b7c84246484d

---
+++ src/it/javalinux/tee/event/XMLEvent.java
@@ -0,0 +1,35 @@
+/*
+ * Created on 21-lug-2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package it.javalinux.tee.event;
+
+import java.util.Date;
+
+public class XMLEvent implements Event {
+	private String xmlString;
+
+	public Date getInterceptTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getInterceptorName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getXmlString() {
+		return xmlString;
+	}
+	
+
+	public void setXmlString(String xmlString) {
+		this.xmlString = xmlString;
+	}
+	
+
+
+}
