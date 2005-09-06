0fcaf554087efe039e2d159ae14b756c6a858b28
maestri <maestri@171577af-f0f8-0310-8ebc-b7c84246484d>
2005-09-06 15:53:06 +0000

    git-svn-id: svn+ssh://svn.lince.it/svn/Tee/trunk@73 171577af-f0f8-0310-8ebc-b7c84246484d

---
+++ src/it/javalinux/tee/interceptor/AOPInterceptorTest.java
@@ -0,0 +1,29 @@
+/*
+ * Created on 5-set-2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package it.javalinux.tee.interceptor;
+
+import it.javalinux.tee.annotations.TeeEvent;
+import it.javalinux.tee.event.MapEvent;
+import it.javalinux.tee.event.TestEvent;
+import junit.framework.TestCase;
+
+public class AOPInterceptorTest /*extends TestCase*/ {
+
+	public static void main(String[] args) {
+		//junit.swingui.TestRunner.run(AOPInterceptorTest.class);
+		AOPInterceptorTest me = new AOPInterceptorTest();
+		me.testConstructorAdvice();
+	}
+
+	public void testConstructorAdvice() {
+		
+		TestEvent map = new TestEvent("aa",new Integer(1),2,new Float(5.5));
+		
+		//this.assertTrue(true);
+	}
+
+}
